e72fec0f4fa4d5b66a2f59dcf030ee3b55444732
CrisGalvezz <166878778+CrisGalvezz@users.noreply.github.com>
2024-06-11 21:34:00 -0400

    mi primer commit y push
    Gracias profe

---
+++ crud_empleados.py
@@ -0,0 +1,9 @@
+import os, json
+os.system("cls")
+
+def cargar_json(url_archivo):
+    with open(url_archivo, 'r') as archivo:
+        return json.load(archivo)
+    
+empleados = cargar_json('empleados.json')
+print(empleados)
